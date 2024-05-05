726497d6caf08b37aed5d2e4acd4ef3d5ee8cdc2
danascape <danascape@gmail.com>
2024-05-05 18:42:43 +0530

    fix and update
    Signed-off-by: danascape <danascape@gmail.com>

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
-from flask import Flask, request
-from flask import jsonify
+from flask import Flask, request, jsonify
+import serial
+import time
 
+app = Flask(__name__, template_folder='templates')
 
-app = Flask(_name_, template_folder='templates')
+# Define the serial port and baud rate
+port = '/dev/ttyUSB0'  # Change this to match your Raspberry Pi's serial port
+baud_rate = 9600
+
 @app.route('/home', methods=['GET'])
 def get():
-    return response;
+    return jsonify(response)
+
+# Initialize serial connection
+ser = serial.Serial(port, baud_rate)
 
 
 @app.route('/', methods=['GET'])
 def query_records():
     tray = request.args.get('tray')
     print(tray)
+
+    # Construct input in the format <8-1 | 10-2>
+    input_value = f"<{tray}-1 | 10-2>"
+
+    # Write data to serial port
+    ser.write(input_value.encode())
+
     res = {
-        'Tray' : tray
+        'Tray' : tray,
+        'Success': True
     }
     return jsonify(res)
 
@@ -22,5 +38,5 @@ response = {
 }
 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
    app.run(port= 5000,debug=True)
